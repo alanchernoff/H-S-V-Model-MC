c36eff182763def33be7d51832ffc03b00960617
alanchernoff <90869944+alanchernoff@users.noreply.github.com>
2021-09-20 23:17:28 -0400

    Update Monte Carlo.R

--- Monte Carlo.R
+++ Monte Carlo.R
@@ -8,21 +8,23 @@ v = 0.04
 eta = 0.5 
 rho = -0.5 
 del = (1/252)/1559 
-sdel = sqrt(del) 
+sdel = sqrt(del)
+srho = sqrt(1-rho^2)
 
 #Simulation
 
 blanks=matrix(0,nrow=10000,ncol=1559)
 y = cbind(matrix(m,nrow=10000,ncol=1),blanks)
 S2 = cbind(matrix(v,nrow=10000,ncol=1),blanks)
-n1 = randn(1000,1560)
-n2 = randn(1000,1560)
-srho = sqrt(1-rho^2)
+W1 = randn(10000,1560)
+n1 = randn(10000,1560)
+W2 = (n2*srho +rho*W1)
+
 
 for (i in 1:1559){
-  S2[,i+1] <- phi*(v - S2[,i])*del + S2[,i] + (n2[,i]*srho*sdel +rho*n1[,i]*sdel)*eta*sqrt(S2[,i])
-  y[,i+1] <- y[,i] + (m - (S2[,i])/2 )*del + sqrt(S2[,i])*n1[,i]*sdel 
-  }
+  S2[,i+1] <- S2[,i] +phi*(v - S2[,i])*del + eta*sqrt(S2[,i])*W2[,i]*sdel
+  y[,i+1] <- y[,i] + (m - (S2[,i])/2 )*del + sqrt(S2[,i])*W1[,i]*sdel 
+}
 
 #Generate data for simulated 20, 10, and 5 minute intervals
 

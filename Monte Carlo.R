9e1b4388584c00c4ba08f5d9daf64bf674aa04bd
alanchernoff <90869944+alanchernoff@users.noreply.github.com>
2021-12-07 00:52:23 -0500

    Update Monte Carlo.R

--- Monte Carlo.R
+++ Monte Carlo.R
@@ -1,23 +1,24 @@
-#Model Parametrs
+#Model Parameters
 
+t=23400 #number of seconds in a 6.5 hour trading day
 m = 0.05 
 phi = 5  
 v = 0.04 
 eta = 0.5 
 rho = -0.5 
-del = (1/252)/1559 
+del = (1/252)/t 
 sdel = sqrt(del)
 srho = sqrt(1-rho^2)
 
 #Simulation set up 
 
-n = 10000 #number of sample paths
+n = 100 #number of simulations
 
-blanks=matrix(0,nrow=n,ncol=1559)
+blanks=matrix(0,nrow=n,ncol=(t-1))
 y = cbind(matrix(m,nrow=n,ncol=1),blanks)
 S2 = cbind(matrix(v,nrow=n,ncol=1),blanks)
-W1 = matrix(rnorm(n*1560),n,1560)
-n1 = matrix(rnorm(n*1560),n,1560)
+W1 = matrix(rnorm(n*t),nrow=n,ncol=t)
+n1 = matrix(rnorm(n*t),nrow=n,ncol=t)
 W2 = (n1*srho +rho*W1)
 
 #To test for the correlation of -0.5 between brownian motions W1 and W2
@@ -26,17 +27,16 @@ W2 = (n1*srho +rho*W1)
 
 #Heston Volatility Equations
 
-for (i in 1:1559){
+for (i in 1:(t-1)){
   S2[,i+1] <- S2[,i] +phi*(v - S2[,i])*del + eta*sqrt(S2[,i])*W2[,i]*sdel
   y[,i+1] <- y[,i] + (m - (S2[,i])/2 )*del + sqrt(S2[,i])*W1[,i]*sdel 
 }
 
-#Generate data for simulated 20, 10, and 5 minute intervals
+#Generate data for simulated 10, and 5 minute intervals
 
-y_78 = y[,seq(1,ncol(y), 20)]
-y_156 = y[,seq(1,ncol(y), 10)]
-y_312 = y[,seq(1,ncol(y), 5)]
+y_10 = y[,seq(1,ncol(y), 600)]
+y_5 = y[,seq(1,ncol(y), 300)]
 
 #Volatility Calculation
 
-IV = (rowSums(S2))*del
+RV = (rowSums(S2))*del
